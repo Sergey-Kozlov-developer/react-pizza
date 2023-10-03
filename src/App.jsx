11d812028111c22f3e88239adaf3e8a5c12af939
Sergey-Kozlov-developer <vmf.serge.kozlov@gmail.com>
2023-10-03 15:53:09 +0300

    Add backend

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,22 @@ import Header from './components/Header.jsx';
 import Categories from './components/Categories.jsx';
 import Sort from './components/Sort.jsx';
 import PizzaBlock from './components/PizzaBlock.jsx';
-import pizzas from './assets/pizza.json';
+import { useEffect, useState } from 'react';
 
 // function Pizza() {}
 function App() {
+  // useState для получения пицц с бэка
+  const [items, setItems] = useState([]);
+
+  // тут useEffect позволяет в запросе делать 1 запрос на бэк
+  // т.е. он откладывает действие
+  // useEffect выполняет код внутри себя один раз если вконце передать пустой массив []
+  useEffect(() => {
+    fetch('https://api.npoint.io/0682b44356ab955eecbc')
+      .then((res) => res.json())
+      .then((arr) => setItems(arr));
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
@@ -18,7 +30,7 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.map((element) => (
+            {items.map((element) => (
               <PizzaBlock key={element.id} {...element} />
             ))}
           </div>
